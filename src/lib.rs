cd3022a16a03c89f17bd81d91e5de4da5fb5a626
4lDO2 <4lDO2@protonmail.com>
2023-09-28 16:39:07 +0200

    New improved API, and switch ABI to libredox.

--- src/lib.rs
+++ src/lib.rs
@@ -1,162 +1,193 @@
-extern crate syscall;
+#![no_std]
 
-use syscall::{
-    Event as SysEvent,
-    EventFlags,
+use core::marker::PhantomData;
+use core::mem::size_of;
+
+use libredox::{Fd, flag, errno};
+pub use libredox::error::{self, Result};
+
+bitflags::bitflags! {
+    #[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+    pub struct EventFlags: usize {
+        const READ = 1;
+        const WRITE = 2;
+    }
+}
+const _: () = {
+    if EventFlags::READ.bits() != syscall::EventFlags::EVENT_READ.bits() {
+        panic!();
+    }
+    if EventFlags::WRITE.bits() != syscall::EventFlags::EVENT_WRITE.bits() {
+        panic!();
+    }
 };
+impl From<syscall::flag::EventFlags> for EventFlags {
+    fn from(value: syscall::flag::EventFlags) -> Self {
+        let mut this = Self::empty();
+        this.set(Self::READ, value.contains(syscall::EventFlags::EVENT_READ));
+        this.set(Self::WRITE, value.contains(syscall::EventFlags::EVENT_WRITE));
+        this
+    }
+}
+impl From<EventFlags> for syscall::flag::EventFlags {
+    fn from(value: EventFlags) -> Self {
+        let mut this = Self::empty();
+        this.set(Self::EVENT_READ, value.contains(EventFlags::READ));
+        this.set(Self::EVENT_WRITE, value.contains(EventFlags::READ));
+        this
+    }
+}
 
-use std::{
-    collections::BTreeMap,
-    fs::File,
-    io::{
-        prelude::*,
-        Result as IOResult,
-        Error as IOError
-    },
-    os::unix::io::{AsRawFd, RawFd},
-    slice,
-    mem
-};
-
-#[derive(Debug, Clone, Copy)]
-pub struct Event {
-    pub fd: RawFd,
+#[non_exhaustive]
+#[derive(Clone, Copy, Debug)]
+pub struct RawEvent {
+    pub fd: usize,
     pub flags: EventFlags,
+    pub user_data: usize,
 }
 
-/// Subscribe `EvenQueue` to events produced by `fd`.
-pub fn subscribe_to_fd(onto: RawFd, fd: RawFd, id: usize) -> IOResult<()> {
-    syscall::write(onto as usize, &SysEvent {
-        id: fd as usize,
-        flags: syscall::EVENT_READ,
-        data: id
-    })
-    .map_err(|x| IOError::from_raw_os_error(x.errno))
-    .map(|_| ())
+pub struct RawEventQueue {
+    inner: Fd,
 }
-
-/// Unsubscribe `EvenQueue` from events produced by `fd`.
-pub fn unsubscribe_from_fd(onto: RawFd, fd: RawFd, id: usize) -> IOResult<()> {
-    syscall::write(onto as usize, &SysEvent {
-        id: fd as usize,
-        flags: syscall::EventFlags::empty(),
-        data: id
-    })
-    .map_err(|x| IOError::from_raw_os_error(x.errno))
-    .map(|_| ())
-}
-
-
-pub struct EventQueue<R, E = IOError> {
-    /// The file to read events from
-    pub file: File,
-    /// A map of registered file descriptors to their handler callbacks
-    callbacks: BTreeMap<usize, (RawFd, Box<dyn FnMut(Event) -> Result<Option<R>, E>>)>,
-    /// An ID counter, ensuring each registered fd gets a unique ID
-    /// (which means the same fd can be registered multiple times)
-    next_id: usize,
-    /// The default callback to call for not-registered FD
-    default_callback: Option<Box<dyn FnMut(Event) -> Result<Option<R>, E>>>,
-}
-
-impl<R, E> EventQueue<R, E>
-where
-    E: From<IOError>,
-{
-    /// Create a new event queue
-    pub fn new() -> IOResult<EventQueue<R, E>> {
-        Ok(EventQueue {
-            file: File::open("event:")?,
-            callbacks: BTreeMap::new(),
-            next_id: 0,
-            default_callback: None,
+impl RawEventQueue {
+    pub fn new() -> Result<Self> {
+        Ok(Self {
+            inner: Fd::open("event:", flag::O_CREAT | flag::O_RDWR | flag::O_CLOEXEC, 0o700)?,
         })
     }
-
-    /// Set the default callback to be called if an event is produced
-    /// by a FD not registered with `add`.
-    pub fn set_default_callback<F>(&mut self, callback: F)
-    where
-        F: FnMut(Event) -> Result<Option<R>, E> + 'static,
-    {
-        self.default_callback = Some(Box::new(callback));
+    /// Subscribe to events produced by `fd`
+    pub fn subscribe(&self, fd: usize, user_data: usize, flags: EventFlags) -> Result<()> {
+        self.inner.write(&syscall::data::Event {
+            id: fd,
+            data: user_data,
+            flags: flags.into(),
+        })?;
+        Ok(())
     }
-
-    /// Add a file to the event queue, calling a callback when an event occurs.
-    /// Returns the event id it got, which can be used to remove or trigger this event.
-    ///
-    /// The callback returns Ok(None) if it wishes to continue the event loop,
-    /// or Ok(Some(R)) to break the event loop and return the value.
-    /// Err can be used to allow the callback to return an error, and break the
-    /// event loop.
-    pub fn add<F: FnMut(Event) -> Result<Option<R>, E> + 'static>(
-        &mut self,
-        fd: RawFd,
-        callback: F,
-    ) -> IOResult<usize> {
-        self.next_id += 1;
-
-        self.callbacks.insert(self.next_id, (fd, Box::new(callback)));
-        subscribe_to_fd(self.file.as_raw_fd(), fd, self.next_id)?;
-
-        Ok(self.next_id)
+    /// Unsubscribe from events produced by `fd`
+    pub fn unsubscribe(&self, fd: usize, flags: EventFlags) -> Result<()> {
+        self.inner.write(&syscall::data::Event {
+            id: fd,
+            flags: flags.into(),
+            data: 0,
+        })?;
+        Ok(())
     }
+}
+impl Iterator for RawEventQueue {
+    type Item = Result<RawEvent>;
 
-    /// Remove a file from the event queue, returning its callback if found
-    pub fn remove(
-        &mut self,
-        id: usize
-    ) -> IOResult<Option<Box<dyn FnMut(Event) -> Result<Option<R>, E>>>> {
-        if let Some((fd, callback)) = self.callbacks.remove(&id) {
-            unsubscribe_from_fd(self.file.as_raw_fd(), fd, id)?;
-            Ok(Some(callback))
-        } else {
-            Ok(None)
-        }
-    }
+    // TODO: next_chunk
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut event = syscall::data::Event::default();
 
-    /// Send an event to a descriptor callback
-    pub fn trigger(&mut self, id: usize, event: Event) -> Result<Option<R>, E> {
-        if let Some((_fd, callback)) = self.callbacks.get_mut(&id) {
-            callback(event)
-        } else if let Some(ref mut callback) = self.default_callback {
-            callback(event)
-        } else {
-            Ok(None)
-        }
-    }
-
-    /// Send an event to all descriptor callbacks, useful for cleaning out buffers after init
-    pub fn trigger_all(&mut self, event: Event) -> Result<Vec<R>, E> {
-        let mut rets = Vec::new();
-        for &mut (_fd, ref mut callback) in self.callbacks.values_mut() {
-            if let Some(ret) = callback(event)? {
-                rets.push(ret);
-            }
-        }
-        Ok(rets)
-    }
-
-    /// Process the event queue until a callback returns Some(R)
-    pub fn run(&mut self) -> Result<R, E> {
         loop {
-            let mut events = [SysEvent::default(); 16];
-            let mut events_buf = unsafe {
-                slice::from_raw_parts_mut(
-                    events.as_mut_ptr() as *mut u8,
-                    events.len() * mem::size_of::<SysEvent>()
-                )
-            };
-            let n = self.file.read(&mut events_buf).map_err(E::from)? / mem::size_of::<SysEvent>();
-            for sysevent in &events[..n] {
-                let event = Event {
-                    fd: sysevent.id as RawFd,
-                    flags: sysevent.flags,
-                };
-                if let Some(ret) = self.trigger(sysevent.data, event)? {
-                    return Ok(ret);
+            match self.inner.read(&mut event) {
+                Ok(0) => return None,
+                Ok(n) => {
+                    debug_assert_eq!(n, size_of::<syscall::data::Event>());
+                    return Some(Ok(RawEvent {
+                        fd: event.id,
+                        flags: event.flags.into(),
+                        user_data: event.data,
+                    }));
                 }
+                Err(error::Error { errno: errno::EINTR }) => continue,
+                Err(err) => return Some(Err(err)),
             }
         }
+    }
+}
+
+#[macro_export]
+macro_rules! user_data {
+    {
+        $vis:vis enum $name:ident {
+            $($variant:ident),*$(,)?
+        }
+    } => {
+        #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+        #[repr(usize)]
+        $vis enum $name {
+            $($variant),*
+        }
+
+        impl $crate::UserData for $name {
+            fn into_user_data(self) -> usize {
+                self as usize
+            }
+            fn from_user_data(raw: usize) -> Self {
+                assert!(raw < [$(Self::$variant),*].len());
+                //assert!(raw < core::mem::variant_count::<$name>());
+
+                unsafe { ::core::mem::transmute(raw) }
+            }
+        }
+    };
+}
+
+pub trait UserData: Clone + Copy {
+    fn into_user_data(self) -> usize;
+    fn from_user_data(user_data: usize) -> Self;
+}
+impl UserData for usize {
+    fn into_user_data(self) -> usize {
+        self
+    }
+    // TODO: make unsafe fn
+    fn from_user_data(user_data: usize) -> Self {
+        user_data
+    }
+}
+/*
+unsafe impl<'a, T> UserData for &'a T {
+    fn into_user_data(self) -> usize {
+        self as *const T as usize
+    }
+    unsafe fn from_user_data(user_data: usize) -> Self {
+        &*(user_data as *const T)
+    }
+}
+*/
+
+#[non_exhaustive]
+pub struct Event<U: UserData> {
+    pub user_data: U,
+    pub flags: EventFlags,
+    pub fd: usize,
+}
+
+pub struct EventQueue<U: UserData> {
+    inner: RawEventQueue,
+
+    // We'll be casting user_data to and from U, so ensure it's invariant.
+    _marker: PhantomData<*mut U>,
+}
+
+impl<U: UserData> EventQueue<U> {
+    /// Create a new event queue
+    pub fn new() -> Result<Self> {
+        Ok(EventQueue {
+            inner: RawEventQueue::new()?,
+            _marker: PhantomData,
+        })
+    }
+    pub fn subscribe(&self, fd: usize, data: U, flags: EventFlags) -> Result<()> {
+        self.inner.subscribe(fd, data.into_user_data(), flags)
+    }
+    pub fn unsubscribe(&self, fd: usize, flags: EventFlags) -> Result<()> {
+        self.inner.unsubscribe(fd, flags)
+    }
+}
+impl<U: UserData> Iterator for EventQueue<U> {
+    type Item = Result<Event<U>>;
+
+    // TODO: next_chunk
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next().map(|res| res.map(|raw| Event {
+            user_data: U::from_user_data(raw.user_data),
+            fd: raw.fd,
+            flags: raw.flags,
+        }))
     }
 }
